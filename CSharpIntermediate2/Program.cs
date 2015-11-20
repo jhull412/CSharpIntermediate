f7fe63fe9483881bcb16e99d0a6d6c1d0f54c66c
Justin Hull <hulljc3@upmc.edu>
2015-11-20 08:11:44 -0500

    working on constructors lecture.

---
+++ CSharpIntermediate2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Justin";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+            Console.WriteLine("hello world");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+            Console.ReadLine();
+        }
+    }
+}
