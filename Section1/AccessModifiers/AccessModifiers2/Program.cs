f8ed5020197f3ee9268c473842860ec34cb4efea
Justin Hull <hulljc3@upmc.edu>
2015-11-22 23:40:32 -0500

    finished lecture 11

---
+++ Section1/AccessModifiers/AccessModifiers2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1985, 2, 2));
+            Console.WriteLine(person.GetBirthdate().ToLongDateString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Section1/Fields/Fields2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(2);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+            customer.Promote();
+            Console.WriteLine(customer.Orders.Count);
+            Console.ReadLine();
+        }
+    }
+}
