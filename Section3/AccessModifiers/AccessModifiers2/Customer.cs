28db6791b587efcbd5a5fdcf4c866e9938647791
Justin Hull <hulljc3@upmc.edu>
2015-12-29 14:43:41 -0500

    foo

---
+++ Section3/AccessModifiers/AccessModifiers2/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers2
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating(myParam: true);
+            if (rating == 0)
+                Console.WriteLine("Promoted to Level 1");
+            else
+                Console.WriteLine("Promoted to Level 2");
+        }
+
+        private int CalculateRating(bool myParam)
+        {
+
+            return 0;
+        }
+
+        protected int ProtectedMethod()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Section3/AccessModifiers/AccessModifiers2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AccessModifiers2
+{
+    public class GoldCustomer : Customer
+    {
+
+        public void OfferVouchar()
+        {
+            this.
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+        }
+    }
+}
